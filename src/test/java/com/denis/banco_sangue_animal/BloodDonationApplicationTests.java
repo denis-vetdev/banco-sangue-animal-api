0701ffd135df801e0e5310a1a500ab9afaa24eac
Denis Oliveira <denis.vetdev@gmail.com>
2025-02-12 19:26:42 -0300

    feat: criação de pastas para organização da api

---
+++ src/test/java/com/denis/banco_sangue_animal/BloodDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denis.banco_sangue_animal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
